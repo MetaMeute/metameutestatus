177667737265f6f4fa18ae4d2b0d68d7d1ae97dc
Nils Schneider <nils@nilsschneider.net>
2013-05-03 15:31:54 +0200

    simple status client in python2.7

---
+++ contrib/status.py
@@ -0,0 +1,30 @@
+import feedparser
+import time
+import sys
+import urllib
+import urllib2
+import getpass
+
+BASE = "https://status.metameute.de/"
+
+if len(sys.argv) > 1:
+  msg = " ".join(sys.argv[1:])
+
+  values = {'source' : getpass.getuser(),
+            'message' : msg,
+            'submit' : "submit" }
+
+  data = urllib.urlencode(values)
+  req = urllib2.Request(BASE, data)
+  req.get_method = lambda: "POST"
+  response = urllib2.urlopen(req)
+  code = response.getcode()
+  if code == 200:
+    print "Message posted!"
+  else:
+    print "An error occured. Return code", code
+    sys.exit(1)
+
+feed = feedparser.parse( BASE + "rss" )
+for item in reversed(feed["items"]):
+  print time.strftime("%d %b %Y %H:%M:%S", item["published_parsed"]), item["title"]
